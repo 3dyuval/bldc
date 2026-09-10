5c61409ff9250538f65e688be22625b56adb9423
Yuval.D <yuvddd@pm.me>
2026-09-10 14:58:44 +0300

    fix: add mod throttle inverse for all adc2 options

--- confgenerator.h
+++ confgenerator.h
@@ -9,7 +9,7 @@
 
 // Constants
 #define MCCONF_SIGNATURE		3154770096
-#define APPCONF_SIGNATURE		296593100
+#define APPCONF_SIGNATURE		130732113
 
 // Functions
 int32_t confgenerator_serialize_mcconf(uint8_t *buffer, const mc_configuration *conf);
